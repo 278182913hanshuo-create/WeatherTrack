e4f7aebd32a5e5300ae41fa72555d2d8a44313b4
278182913hanshuo-create <278182913hanshuo@gmail.com>
2026-03-23 16:29:48 +0800

    Add setup_dataset.bat for dataset setup

---
+++ setup_dataset.bat
@@ -0,0 +1,7 @@
+@echo off
+REM WeatherTrack Dataset Setup Script
+
+echo Setting up dataset for WeatherTrack...
+echo Current Date and Time: 2026-03-23 08:23:39
+echo Current User's Login: 278182913hanshuo-create
+echo Dataset setup completed.
